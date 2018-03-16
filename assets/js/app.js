279679251deda209099bcbef16ac88737a1daeaa
Donald Linnell <dlinnell@donalds-air.attlocal.net>
2018-03-16 15:32:31 -0500

    Adding map to page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,15 @@
+function myMap() {
+
+    console.log('starting');
+    var mapProp = {
+        center: new google.maps.LatLng(51.508742, -0.120850),
+        zoom: 5,
+    };
+    var mapLocate = document.getElementById("map");
+    var map = new google.maps.Map(mapLocate, mapProp);
+}
+
 $(document).ready(function () {
-
-    function myMap() {
-        var mapProp = {
-            center: new google.maps.LatLng(51.508742, -0.120850),
-            zoom: 5,
-        };
-        var map = new google.maps.Map(document.getElementById("map"), mapProp);
-    }
-
-    myMap();
-
 
 
 
