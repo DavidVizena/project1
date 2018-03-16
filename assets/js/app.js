8b06788efb7c94d91e97ec784b3c8899afa1d958
Donald Linnell <dlinnell@donalds-air.attlocal.net>
2018-03-16 14:44:20 -0500

    Begin adding map to pack

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
 
+    function myMap() {
+        var mapProp = {
+            center: new google.maps.LatLng(51.508742, -0.120850),
+            zoom: 5,
+        };
+        var map = new google.maps.Map(document.getElementById("map"), mapProp);
+    }
+
+    myMap();
 
 
 
