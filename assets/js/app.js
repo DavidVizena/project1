440e5342d40ab519653222d30674d6279500f76a
Donald Linnell <dlinnell@donalds-air.attlocal.net>
2018-03-16 15:39:20 -0500

    Centers map on Houston

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,8 @@ function myMap() {
 
     console.log('starting');
     var mapProp = {
-        center: new google.maps.LatLng(51.508742, -0.120850),
-        zoom: 5,
+        center: new google.maps.LatLng(29.756846, -95.363444),
+        zoom: 10,
     };
     var mapLocate = document.getElementById("map");
     var map = new google.maps.Map(mapLocate, mapProp);
