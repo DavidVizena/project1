fe003669bcd076878233b03b0405c655b8b77cd9
Donald Linnell <dlinnell@donalds-air.attlocal.net>
2018-03-16 16:39:48 -0500

    Displays marker on map of new location once add location button is pressed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+var map;
+
 // Function to be called by Google Maps on callback, outside document.ready to prevent issues with load
 function displayMap() {
 
@@ -6,30 +8,55 @@ function displayMap() {
         zoom: 10,
     };
     var mapLocate = document.getElementById("map");
-    var map = new google.maps.Map(mapLocate, mapProp);
+    map = new google.maps.Map(mapLocate, mapProp);
 }
 
 $(document).ready(function () {
 
-    function newLocation(){
+    function newLocation() {
 
         event.preventDefault();
 
-        if ($('#location').val()){
+        // Verify that something has been entered
+        if ($('#location').val()) {
 
-        var location = $('#location').val().trim()
+            // Create a new div containing the location entered and append that to the addedLocations div on page, then clear form
+            var location = $('#location').val().trim()
+            var newDiv = $('<div>').text(location);
+            newDiv.appendTo('#addedLocations');
 
-        $('#location').val('');
+            $('#location').val('');
 
-        var newDiv = $('<div>').text(location);
-        newDiv.appendTo('#addedLocations');
+            var queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&key=AIzaSyDqVvFEbKT3bghZxOT581eUo156nRZR4bw';
+
+            console.log(queryURL);
+
+            $.ajax({
+                url: queryURL,
+                method: 'Get'
+
+
+            }).then(function (res) {
+            
+                var latLong = res.results[0].geometry.location;
+                var locationLat = latLong.lat;
+                var locationLong = latLong.lng;
+
+                var locationCordinates = {lat: locationLat, lng: locationLong};
+
+                var newMarker = new google.maps.Marker({position: locationCordinates});
+
+                newMarker.setMap(map);
+
+            });
 
         };
+
     };
 
-    $('#addLocation').on('click', newLocation);
+        $('#addLocation').on('click', newLocation);
 
 
 
-    // Closing tag for document.ready
-});
+        // Closing tag for document.ready
+    });
