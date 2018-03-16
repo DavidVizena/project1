7dfdb4116954387f848ffaeae1ea8981094af9ea
Donald Linnell <dlinnell@donalds-air.attlocal.net>
2018-03-16 15:49:24 -0500

    Adds new address from location search to added locations on page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-function myMap() {
+// Function to be called by Google Maps on callback, outside document.ready to prevent issues with load
+function displayMap() {
 
-    console.log('starting');
     var mapProp = {
         center: new google.maps.LatLng(29.756846, -95.363444),
         zoom: 10,
@@ -10,6 +10,24 @@ function myMap() {
 }
 
 $(document).ready(function () {
+
+    function newLocation(){
+
+        event.preventDefault();
+
+        if ($('#location').val()){
+
+        var location = $('#location').val().trim()
+
+        $('#location').val('');
+
+        var newDiv = $('<div>').text(location);
+        newDiv.appendTo('#addedLocations');
+
+        };
+    };
+
+    $('#addLocation').on('click', newLocation);
 
 
 
