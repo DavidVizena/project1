181627c6f2a8379b3c499eb8894a70bdc60c36a7
Rovch <wesleyjacksonjr33@hotmail.com>
2018-03-18 17:19:02 -0500

    created and just about finished the entire sign in, user profile logic for the back end, you can login,sign in and sign up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
-$(document).ready(function () {
 
+$(document).ready(function () {
     var locations = [];
     var center;
     var map;
+    var userStatus = false;
+    var panelStatus = false;
 
-
+    statusChecker();
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyCmsUyEaY8znd7KUyIoiyVkl2SHNPa-Bnw",
@@ -15,25 +17,146 @@ $(document).ready(function () {
         messagingSenderId: "876221643870"
     };
     firebase.initializeApp(config);
-
     db = firebase.database();
 
-    function submitData() {
+    statusChecker();
+    function statusChecker(activator1, activator2) {
+        userStatus = activator1;
+        panelStatus = activator2;
+        if (userStatus) {
+            var logoutBtn = $("#logoutBtn").show();
+            $("#userP").hide();
+            $("#formEmail").hide();
+            $("#formPassword").hide();
+            $("#formConfirmPassword").hide();
+            $("#formUsername").hide();
+            $("#signUpBtn").hide();
+            $("#loginBtn").hide();
+            $("#signInBtn").hide();
+        } else if (!userStatus) {
+            $("#userCard").text("Sign Up Today");
+            $("#logoutBtn").hide();
+            $("#userP").show();
+            $("#formEmail").show();
+            $("#formPassword").show();
+            $("#formConfirmPassword").show();
+            $("#formUsername").show();
+            $("#signUpBtn").show();
+            $("#loginBtn").hide();
+            $("#signInBtn").show();
+            
+        } 
 
-        // add it to a javascript object
-        var data = {
-            userName: "",
-            email: "",
-            pass: "",
-        }
-
-        //pushes data obj to firebase node "newTrain"
-        var ref = db.ref("users");
-        ref.push(data);
-        ref.on("value", getData);
+        if (panelStatus) {
+            $("#userCard").text("Login");
+            $("#formUsername").hide();
+            $("#userP").hide();
+            $("#signUpBtn").hide();
+            $("#signInBtn").hide();
+            $("#formConfirmPassword").hide();
+            $("#loginBtn").show();
+        } 
     }
 
+    // all buttons used for signIn, logout, and login functionality
+    $("#signInBtn").on("click", function (e) {
+        e.preventDefault();
+        statusChecker(false, true);
+    });
 
+    $("#signUpBtn").on("click", function (e) {
+        e.preventDefault();
+        signUp();
+    });
+
+    $("#loginBtn").on("click", function (e) {
+        e.preventDefault();
+        login();
+    });
+
+    $("#logoutBtn").on("click", function (e) {
+        e.preventDefault();
+        logout();
+    });
+
+    // checks the current users state and if anything is changed do something
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            var user = firebase.auth().currentUser;
+            console.log(user);
+            console.log("logged in as: " + user.email);
+            statusChecker(true),false;
+            var email_id = user.email;
+            var editGreeting = $("#userCard").text("Welcome User : " + user.email);
+        } else {
+            // No user is signed in.
+            console.log("No user detected, Please sign in.");
+            statusChecker(false, false);
+        }
+    });
+
+    // sign up function
+    function signUp() {
+
+        // grabs data from input fields
+        var userEmail = $("#formEmail").val().trim();
+        var userPass = $("#formPassword").val().trim();
+        var confirmPass = $("#formConfirmPassword").val().trim();
+        userName = $("#formUsername").val().trim();
+        console.log(userEmail);
+
+        if (userPass !== confirmPass) {
+            alert("Sorry! your passwords do not match, please fix them to continue")
+        } else {
+            firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
+            .then(function(){
+                signInAuth()
+            }).catch(function (error) {
+                //TODO: had to put user display name assigning here due to asynchronous issues will tidy up later
+                firebase.auth().currentUser.updateProfile({
+                    displayName: userName,
+                  }).then(function() {
+                //   console.log(user.userName);
+                  });
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log("CreateUserError : " + errorMessage);
+                // signInAuth();
+            });
+        }
+    }
+
+    function signInAuth() {
+        firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("SignInError : " + errorMessage);
+            console.log("cool");
+        });
+    }
+
+    // login function
+    function login() {
+        // grabs data from input fields
+        var userEmail = $("#formEmail").val().trim();
+        var userPass = $("#formPassword").val().trim();
+        console.log(userEmail);
+        firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("SignInError : " + errorMessage);
+        });
+    }
+
+    // logout function
+    function logout() {
+        firebase.auth().signOut();
+        statusChecker(true, false);
+    }
 
 
     function newLocation() {
@@ -116,8 +239,6 @@ $(document).ready(function () {
     function makeMap() {
 
         $.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyDqVvFEbKT3bghZxOT581eUo156nRZR4bw', function () {
-
-            console.log('success');
             var mapProp = {
                 center: new google.maps.LatLng(29.756846, -95.363444),
                 zoom: 10,
