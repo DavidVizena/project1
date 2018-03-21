b5f08b51817fb6dbdf8ff971f46770f5d71a8dc4
Vizena2005 <vizena2005@gmail.com>
2018-03-20 19:43:08 -0500

    adding transition to the login

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,6 +33,14 @@ $(document).ready(function () {
         panelStatus = activator2;
         if (userStatus) {
             var logoutBtn = $("#logoutBtn").show();
+            $("#homeSection").fadeOut(1000, function () {
+                $("body").css({"overflow-y" : "hidden"});
+                $("body").css({"max-height" : "500px"});
+                $("body").css({"transition-property" : "all"});
+                $("body").css({"transition-duration" : ".5s"});
+                $("body").css({"transition-timing-function" : "cubic-bezier(0, 1, 0.5, 1)"});
+                $(this).hide();
+            });
             $("#userP").hide();
             $("#formEmail").hide();
             $("#formPassword").hide();
@@ -42,6 +50,7 @@ $(document).ready(function () {
             $("#loginBtn").hide();
             $("#signInBtn").hide();
         } else if (!userStatus) {
+            $("#homeSection").show();
             $("#userCard").text("Sign Up Today");
             $("#logoutBtn").hide();
             $("#userP").show();
@@ -239,31 +248,31 @@ $(document).ready(function () {
 
     };
 
-    function findPlaces () {
+    function findPlaces() {
 
         $.ajax({
             method: 'GET',
             url: 'https://developers.zomato.com/api/v2.1/geocode?lat=30&lon=-95',
-            headers: {'user-key' : '9b0f7f04f6701a9e6b5c0b40c2a61b80'},
-        }).then(function(res){
+            headers: { 'user-key': '9b0f7f04f6701a9e6b5c0b40c2a61b80' },
+        }).then(function (res) {
             console.log(res);
         })
 
-       /*  var parameters = {
-            location: centerPoint,
-            radius: 1000,
-            type: ['restaurant']
-        }
-
-        placesSearch = new google.maps.places.PlacesService(map);
-        placesSearch.nearbySearch(parameters, function(res){
-            console.log(res);
-
-            for (var i=0; i <= 4; i++){
-                var newMarker = new google.maps.Marker({ position: res[i].geometry.location });
-                newMarker.setMap(map);
-            };
-        }); */
+        /*  var parameters = {
+             location: centerPoint,
+             radius: 1000,
+             type: ['restaurant']
+         }
+ 
+         placesSearch = new google.maps.places.PlacesService(map);
+         placesSearch.nearbySearch(parameters, function(res){
+             console.log(res);
+ 
+             for (var i=0; i <= 4; i++){
+                 var newMarker = new google.maps.Marker({ position: res[i].geometry.location });
+                 newMarker.setMap(map);
+             };
+         }); */
 
     };
 
