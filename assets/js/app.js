7eaf0d12a53a8b1eb13c25e91534009633b17952
Vizena2005 <vizena2005@gmail.com>
2018-03-22 13:46:28 -0500

    adding some jquery that will make smooth transitions to sections when clicked in the nav bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -64,6 +64,20 @@ $(document).ready(function () {
     sr.reveal('#mainFooter', {
         move: 0
     });
+
+    $('a[href*="#"]:not([href="#"])').click(function(){
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')&& location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $(' [name' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    })
+
     // 
 
 
